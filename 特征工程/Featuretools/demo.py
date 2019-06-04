c472b6ae1fcd8b31c5f1f99182fa72eb30f280fb
taoshilai <taoshilai@x3platform.com>
2019-06-04 10:19:29 +0800

    add featuretools exampl.ipynb

--- 特征工程/Featuretools/demo.py
+++ 特征工程/Featuretools/demo.py
@@ -5,16 +5,27 @@
 @time: 2019/1/31 11:26
 @desc: featuretools使用
 """
-
+path='../../users.xlsx'
+import pandas as pd
+clients=pd.read_excel(path,sheet_name='clients')
+loans=pd.read_excel(path,sheet_name='loans')
+payments=pd.read_excel(path,sheet_name='payments')
+# print('clients',clients)
+# print('loans',loans)
+# print('payments',payments)
 import featuretools as ft
 
-# Create new entityset
+# Create new entityset#创建一个空的实体集
 es = ft.EntitySet(id='clients')
+#要把多个实体进行合并。每个实体必须带有一个索引，即所有元素都唯一的数据列
+#clients 数据框（dataframe）的索引是 client_id
 es = es.entity_from_dataframe(entity_id='clients', dataframe=clients,
                               index='client_id', time_index='joined')
+print('es clients',es)
+#loans 数据框也有唯一索引 loan_id，将其加入实体集的语法和处理 clients 的语法相同
 es = es.entity_from_dataframe(entity_id='loans', dataframe=loans,
-                              index='loans_id', time_index='joined')
-
+                              index='loans_id', time_index='loan_start')
+print('es loans',es)
 # 对于没有唯一索引的表：我们需要传入参数make_index = True并指定索引的名称。
 # 另外，虽然featuretools会自动推断实体中每个列的数据类型，但我们可以通过将列类型的字典传递给参数variable_types来覆盖它。
 es = es.entity_from_dataframe(entity_id='payments', dataframe=payments,
@@ -23,7 +34,7 @@ es = es.entity_from_dataframe(entity_id='payments', dataframe=payments,
 # 指定Table Relationships。在执行聚合时，我们按父变量对子表进行分组，并计算每个父表的子表的统计信息。
 # 要在featuretools中形式化关系，只需指定将两个表链接在一起的变量。客户端和loans表通过client_id变量链接，贷款和付款与loan_id链接。
 # 创建关系并将其添加到entityset的语法如下所示:
-
+print('es payments',payments)
 r_client_previous = ft.Relationship(es['clients']['client_id'], es['loans']['client_id'])
 
 # Add the relationship to the entity set
